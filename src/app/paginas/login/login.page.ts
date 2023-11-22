2d896c6986968c6aee82add9b142b5c7948f07f7
AlexZander25 <aledelflou@hotmail.cl>
2023-11-22 02:00:02 -0300

    holi

--- src/app/paginas/login/login.page.ts
+++ src/app/paginas/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private menuController: MenuController,
+    private alertController: AlertController) { }
 
   ngOnInit() {
   }
+  mostrarMenu() {
+    this.menuController.open('first');
+  }
 
+
+  async Enviar() {
+    const alert = await this.alertController.create({
+      header: 'Ingreso correctamente',
+      message: 'Bienvenido a Duoc UC!',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+
+
+  }
 }
